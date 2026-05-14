36f6efe3d27b6387601022863872642e585810c5
abhiramSharma2002 <abhiramsharma2002@gmail.com>
2026-05-14 11:26:58 +0530

    Updated db config for cloud

--- db.js
+++ db.js
@@ -6,14 +6,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 24127,
     ssl: {
-        rejectUnauthorized: false 
+        rejectUnauthorized: false
     },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-const db = pool.promise();
-module.exports = db;
+module.exports = pool.promise();
